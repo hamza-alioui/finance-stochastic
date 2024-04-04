40073929612b8ee885fc24c8ba0a8539462e4411
breno-st <b_tiburcio@hotmail.com>
2024-04-04 15:49:02 +0200

    Part 2 questions 1 and 2 imrpoved

---
+++ src/Plan.py
@@ -0,0 +1,85 @@
+
+ ## TODO Code before writting:
+
+
+# ## TODO Writting:
+#4h### Write Data Collections 07/03
+#4h### Write Conclusion part 7  by 27/11
+#8h### Generate all results vizualizations by 04/12
+#4h### Write Following Steps part 6
+#4h### Re-Write Introduction part 1  11/12
+#68###
+
+# ## TODO Code while writting:
+# ### Turn big-citis-transpot into App:
+# ### Connect and improve codes to build an app (example 2)
+# ### Example 2: Application Development (Databases, Django)
+# ### Create code for GloSS
+
+# ### Desing Finance Application
+
+## Run 5h/3, Bike 10h/4, Gym 5h (back, chest, leg, core, core)
+## week:1   12-12-21 free
+## week:2   16-12-24 free
+## week:3   14-14-30 speed
+## week:4   16-16-10-26 transition
+## week:5   16-16-12-30 transition
+## week:6   16-8-16-8-10-24 transition
+## week:7   14-8-14-8-21-28 volume
+## week:8   10-10
+
+# # run, bike, gym: 4/4/5
+# # proj,lang, read,
+# # cook, clea, clot, mrkt
+
+
+
+
+#3## Mecredi:    5h: bike, 8h: work, 12h: work, 14h: work, 18h: ----, 20h: chst, 21h: ----, 22h: read		>>> Submit Stoc Finan (Part 1).
+# ## Jeudi:      5h: ru12, 8h: Work, 12h:lunch, 14h: Work, 18h: cook, 20h: proj, 21h: proj, 22h: read		>>> Conclusion,
+# ## Vendredi:   5h: bike, 8h: work, 12h: work, 14h: work, 18h: ----, 20h: back, 21h: ----, 22h: read		>>> Finish Conclusion 05/04.
+# ## Samedi:     5h: bike, 8h: room, 12h: mrkt, 14h: proj, 18h: proj, 20h: ----, 21h: ----, 22h: ----   	>>>
+# ## Dimache:    5h: bike, 8h: clot, 12h: cook, 14h: proj, 18h: ru24, 20h: ----, 21h: ----, 22h: ----		>>>
+# ## Lundi:      5h: ----, 8h: work, 12h: work, 14h: work, 18h: ru1x, 20h: legs, 21h: lang, 22h: read		>>> Financ Stoc Meeting
+# ## Mardi:      5h: work, 8h: proj, 12h: proj, 14h: proj, 18h: ru1x, 20h: core, 21h: proj, 22h: read		>>> Stoc Finan (Part 2)
+#0## Mecredi:    5h: bike, 8h: work, 12h: work, 14h: work, 18h: chst, 20h: ----, 21h: proj, 22h: read		>>> Submit Stoc Finan (Part 2).
+# ## Jeudi:      5h: ru12, 8h: Work, 12h:lunch, 14h: Work, 18h: cook, 20h: proj, 21h: proj, 22h: read		>>> Data Collection,
+# ## Vendredi:   5h: bike, 8h: work, 12h: work, 14h: work, 18h: ----, 20h: back, 21h: ----, 22h: read		>>> Finish Data Collection 12/04.
+# ## Samedi:     5h: bike, 8h: room, 12h: mrkt, 14h: proj, 18h: proj, 20h: ----, 21h: ----, 22h: ----   	>>> Following steps,
+# ## Dimache:    5h: bike, 8h: clot, 12h: cook, 14h: proj, 18h: ru24, 20h: ----, 21h: ----, 22h: ----		>>> Following steps,
+# ## Lundi:      5h: ----, 8h: work, 12h: work, 14h: work, 18h: ru1x, 20h: legs, 21h: lang, 22h: read		>>> Finish Following steps 15/04.
+# ## Mardi:      5h: work, 8h: proj, 12h: proj, 14h: proj, 18h: ru1x, 20h: core, 21h: proj, 22h: read		>>> Introduction,
+#7## Mecredi:    5h: bike, 8h: work, 12h: work, 14h: work, 18h: chst, 20h: ----, 21h: proj, 22h: read		>>> Finish Introduction 19/04 & submit for review.
+# ## Jeudi:      5h: ru12, 8h: Work, 12h:lunch, 14h: Work, 18h: cook, 20h: proj, 21h: proj, 22h: read		>>> Review Part 1,
+# ## Vendredi:   5h: bike, 8h: work, 12h: work, 14h: work, 18h: back, 20h: ----, 21h: proj, 22h: read		>>> Review Part 1.
+# ## Samedi:     5h: bike, 8h: room, 12h: mrkt, 14h: proj, 18h: proj, 20h: ----, 21h: proj, 22h: ----   	>>> LBL Race
+# ## Dimache:    5h: bike, 8h: clot, 12h: cook, 14h: proj, 18h: ru24, 20h: ----, 21h: proj, 22h: ----		>>>
+# ## Lundi:      5h: ----, 8h: work, 12h: work, 14h: work, 18h: ----, 20h: legs, 21h: lang, 22h: read		>>>
+# ## Mardi:      5h: ----, 8h: work, 12h: work, 14h: work, 18h: ru10, 20h: core, 21h: proj, 22h: read		>>>
+
+
+# ## TODO PLAN
+# ## Savings: Camila Trip (N26), Mobile (Proximus), Laptop
+# ## Nutrition: Creatina, aronia,
+
+# ## Obstacles (0/5): Submit Stoc Finan (15/04)
+# ## Obstacles (0/5): Submit thesis (???)
+# ## Obstacles (0/5): Exam Stoc Finan (One week review)
+# ## Obstacles (0/5): Exam Info/Coding (One week study)
+# ## Obstacles (0/5): Exam Linguistic (print slides and books to study in Alps Code as well)
+
+
+# ## TODO SHORT
+# ## Bike Race LBL 160KM (20/04)
+# ## Guests in BXL: Marcia e Cal (26/04 - 1/05)
+# ## Trip to Lisbon (03/05-09/05)
+# ## Trip to London (17/05-21/05)
+# ## Running Race 20km Brussels (26/05)
+# ## Alpes (28/06 - 07/07)
+
+# ## Carte Profissionelle:
+# ## Business counters contact:
+# ### - UCM: meeting on wednesday21st,  Avenue LouiseBusiness plan, motivational letter,
+# ## Accountant:
+# ### - Yelin
+# ### -
